3a9ead9b50e2602c880b9f55134c875d05b6808a
Soumaia Aloui <soumaiaalouii@gmail.com>
2026-01-02 17:31:17 +0100

    connect frontend to production backend

---
+++ src/services/taskService.js
@@ -0,0 +1,30 @@
+const API_URL = "http://localhost:5001/tasks";
+
+export const getTasks = async () => {
+  const res = await fetch(API_URL);
+  if (!res.ok) throw new Error("Impossible de récupérer les tâches");
+  return res.json();
+};
+
+export const addTask = async (task) => {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(task),
+  });
+  return res.json();
+};
+
+export const deleteTask = async (id) => {
+  await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+};
+
+export const toggleTask = async (task) => {
+  const res = await fetch(`${API_URL}/${task.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ ...task, completed: !task.completed }),
+  });
+  return res.json();
+};
+
